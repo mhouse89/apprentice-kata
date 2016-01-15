3a4b114c22d3d0a03bee94bd431635f55db7acaa
mhouse89 <matthew.house89@gmail.com>
2016-01-15 11:00:56 -0500

    Did some refactoring to clean up the fizzbuzz spec code

--- FizzBuzz/javascript/spec/FizzBuzzSpec.js
+++ FizzBuzz/javascript/spec/FizzBuzzSpec.js
@@ -1,17 +1,18 @@
 describe("The Fizzbuzz function", function() {
   var fizzbuzz;
+  var result;
 
   beforeEach(function(){
     fizzbuzz = new Fizzbuzz;
   });
 
   it("returns 1 when passed a 1", function() {
-    var result = fizzbuzz.convert(1);
+    result = fizzbuzz.convert(1);
     expect(result).toBe(1);
   });
 
   it("returns 2 when passed a 2", function() {
-    var result = fizzbuzz.convert(2);
+    result = fizzbuzz.convert(2);
     expect(result).toBe(2);
   });
 
