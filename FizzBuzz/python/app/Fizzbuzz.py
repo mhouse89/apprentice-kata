8e6d01b10ec031e6e30b0609909b84d4f8031821
mhouse89 <matthew.house89@gmail.com>
2016-01-15 15:01:54 -0500

    Fizzbuzz returns Fizz when passed a 3

--- FizzBuzz/python/app/Fizzbuzz.py
+++ FizzBuzz/python/app/Fizzbuzz.py
@@ -1,3 +1,5 @@
 class Fizzbuzz(object):
     def convert(self, number):
+        if (number == 3):
+            return "Fizz"
         return number
--- FizzBuzz/python/test/test_Fizzbuzz.py
+++ FizzBuzz/python/test/test_Fizzbuzz.py
@@ -5,9 +5,15 @@ class FizzbuzzTester(unittest.TestCase):
 
     def setUp(self):
         self.fizzbuzz = Fizzbuzz()
+
     def test_fizzbuzz_returns_1_when_passed_1(self):
         result = self.fizzbuzz.convert(1)
         self.assertEqual(1, result)
+
     def test_fizzbuzz_returns_2_when_passed_2(self):
         result = self.fizzbuzz.convert(2)
         self.assertEqual(2, result)
+
+    def test_fizzbuzz_returns_Fizz_when_passed_2(self):
+        result = self.fizzbuzz.convert(3)
+        self.assertEqual("Fizz", result)
