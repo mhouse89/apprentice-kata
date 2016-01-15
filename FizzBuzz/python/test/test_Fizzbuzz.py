56de88bcbbbdcc3a3f17bd92f938b2e5cec04d89
mhouse89 <matthew.house89@gmail.com>
2016-01-15 14:59:09 -0500

    refactored code to clean up the test methods

--- FizzBuzz/python/test/test_Fizzbuzz.py
+++ FizzBuzz/python/test/test_Fizzbuzz.py
@@ -2,11 +2,12 @@ import unittest
 from app.Fizzbuzz import Fizzbuzz
 
 class FizzbuzzTester(unittest.TestCase):
+
+    def setUp(self):
+        self.fizzbuzz = Fizzbuzz()
     def test_fizzbuzz_returns_1_when_passed_1(self):
-        fizzbuzz = Fizzbuzz()
-        result = fizzbuzz.convert(1)
+        result = self.fizzbuzz.convert(1)
         self.assertEqual(1, result)
     def test_fizzbuzz_returns_2_when_passed_2(self):
-        fizzbuzz = Fizzbuzz()
-        result = fizzbuzz.convert(2)
+        result = self.fizzbuzz.convert(2)
         self.assertEqual(2, result)
