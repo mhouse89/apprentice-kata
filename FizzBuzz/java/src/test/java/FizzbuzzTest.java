33eb1ced4c6651d1337c3462fbf6f2172e5a7b93
mhouse89 <matthew.house89@gmail.com>
2016-01-14 21:42:37 -0500

    Did some refactoring to clean up the test code

--- FizzBuzz/java/src/test/java/FizzbuzzTest.java
+++ FizzBuzz/java/src/test/java/FizzbuzzTest.java
@@ -1,23 +1,30 @@
 import static org.junit.Assert.assertEquals;
 
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 public class FizzbuzzTest {
 
+  String result;
+  Fizzbuzz fb;
+
+  @Before
+  public void setUp() {
+          fb = new Fizzbuzz();
+  }
+
+
   @Test
   public void fizzbuzzReturns1WhenPassedTheNumber1() {
-    Fizzbuzz fb = new Fizzbuzz();
-    String result = fb.convert(1);
-    assertEquals("1", result);
+          result = fb.convert(1);
+          assertEquals("1", result);
   }
 
   @Test
   public void fizzbuzzReturns2WhenPassedTheNumber2() {
-    Fizzbuzz fb = new Fizzbuzz();
-    String result = fb.convert(2);
-    assertEquals("2", result);
+          result = fb.convert(2);
+          assertEquals("2", result);
   }
 
 }
