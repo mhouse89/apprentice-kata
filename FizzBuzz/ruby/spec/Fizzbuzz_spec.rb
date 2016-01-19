bf237fb20920c6ced368f37edbfa8826a70a6b8f
mhouse89 <matthew.house89@gmail.com>
2016-01-19 13:54:00 -0500

    Fizzbuzz returns Fizz when passed a 3

--- FizzBuzz/ruby/spec/Fizzbuzz_spec.rb
+++ FizzBuzz/ruby/spec/Fizzbuzz_spec.rb
@@ -8,6 +8,19 @@ describe FizzBuzz do
           expect(FizzBuzz.convert(1)).to eql(1)
         end
       end
+
+      context "given a 2" do
+        it "returns 2" do
+          expect(FizzBuzz.convert(2)).to eql(2)
+        end
+      end
+
+      context "given a 3" do
+        it "returns Fizz" do
+          expect(FizzBuzz.convert(3)).to eql("Fizz")
+        end
+      end
+
     end
 
 end
--- FizzBuzz/ruby/src/Fizzbuzz.rb
+++ FizzBuzz/ruby/src/Fizzbuzz.rb
@@ -1,3 +1,10 @@
 class FizzBuzz
 
+  def self.convert(number)
+    if number == 3
+      return "Fizz"
+    end
+    else
+      return number
+  end
 end
