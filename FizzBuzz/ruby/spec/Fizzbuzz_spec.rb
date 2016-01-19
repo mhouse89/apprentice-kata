e9919b099c2a50f6f945f9f7232ecf8175175cb9
mhouse89 <matthew.house89@gmail.com>
2016-01-19 13:12:32 -0500

    Tests were set up wrong... now I'm getting failing tests

--- FizzBuzz/ruby/spec/Fizzbuzz_spec.rb
+++ FizzBuzz/ruby/spec/Fizzbuzz_spec.rb
@@ -1,5 +1,13 @@
-require "Fizzbuzz"
+require_relative '../src/fizzbuzz'
 
-describe Fizzbuzz do
+describe FizzBuzz do
+
+  describe ".convert" do
+      context "given a 1" do
+        it "returns 1" do
+          expect(FizzBuzz.convert(1)).to eql(1)
+        end
+      end
+    end
 
 end
