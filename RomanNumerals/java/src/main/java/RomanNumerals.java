69c3de6e73becad59c1aa7acd8d113120ceb61f6
mhouse89 <matthew.house89@gmail.com>
2016-01-21 15:02:09 -0500

    Recactored the test and prod code a little to clean things up.

--- RomanNumerals/java/src/main/java/RomanNumerals.java
+++ RomanNumerals/java/src/main/java/RomanNumerals.java
@@ -1,24 +1,23 @@
 import java.util.Hashtable;
 
 public class RomanNumerals {
+public static int convert(String romanNumeral) {
 
-private static StringBuilder romanNumeralBuffer;
-private static int total;
-private static int index;
-private static Hashtable<Character, Integer> conversion =
-  new Hashtable<Character, Integer>();
+        StringBuilder romanNumeralBuffer;
+        int total;
+        int index;
+        Hashtable<Character, Integer> conversion =
+                new Hashtable<Character, Integer>();
+        conversion.put('I', 1);
+        
+        romanNumeralBuffer = new StringBuilder(romanNumeral);
+        total = 0;
 
+        for(index = 0; index < romanNumeralBuffer.length(); index++) {
+                total += conversion.get(romanNumeralBuffer.charAt(index));
+        }
 
-  public static int convert(String romanNumeral) {
-    conversion.put('I', 1);
-    romanNumeralBuffer = new StringBuilder(romanNumeral);
-    total = 0;
-
-    for(index = 0; index < romanNumeralBuffer.length(); index++) {
-      total += conversion.get(romanNumeralBuffer.charAt(index));
-    }
-
-    return total;
-  }
+        return total;
+}
 
 }
--- RomanNumerals/java/src/test/java/RomanNumeralsTest.java
+++ RomanNumerals/java/src/test/java/RomanNumeralsTest.java
@@ -6,18 +6,27 @@ import org.junit.Test;
 
 public class RomanNumeralsTest {
 
-  @Test
-  public void testRomanNumeralsConvertsIto1() throws Exception {
-    int result = RomanNumerals.convert("I");
-    assertEquals(1, result);
+int result;
 
-  }
+@Test
+public void testRomanNumeralsConvertsIto1() throws Exception {
+        result = RomanNumerals.convert("I");
+        assertEquals(1, result);
 
-  @Test
-  public void testRomanNumeralsConvertsIIto2() throws Exception {
-    int result = RomanNumerals.convert("II");
-    assertEquals(2, result);
-    
-  }
+}
+
+@Test
+public void testRomanNumeralsConvertsIIto2() throws Exception {
+        result = RomanNumerals.convert("II");
+        assertEquals(2, result);
+
+}
+
+@Test
+public void testRomanNumeralsConvertsIIIto3() throws Exception {
+        result = RomanNumerals.convert("III");
+        assertEquals(3, result);
+
+}
 
 }
