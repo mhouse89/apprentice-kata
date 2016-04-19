da8d57b75d82ac93fb6d2e4fdef777399125b25f
Matt House <matthew.house89@gmail.com>
2016-04-19 09:57:56 -0400

    adding babysitter stuff

--- bsitterkata/src/main/java/BabySitter.java
+++ bsitterkata/src/main/java/BabySitter.java
@@ -1,7 +1,52 @@
-package main.java;
 
-/**
- * Created by matthewhouse on 4/14/16.
- */
 public class BabySitter {
+
+    private static final int UPPER_END_TIME_THRESHOLD = 4;
+    private static final int LOWER_START_TIME_THRESHOLD = 17;
+    private static final int MIDNIGHT = 24;
+
+    private static final int START_TO_BED = 12;
+    private static final int BED_TO_MIDNIGHT = 8;
+    private static final int MIDNIGHT_TO_END = 16;
+
+
+    public int calculateNightlyCharge(int startTime, int bedTime, int endTime) {
+
+        int nightlyCharge = 0;
+
+        if (timeIsValid(startTime, endTime)) {
+
+            nightlyCharge += startToBedtime(startTime, bedTime);
+            nightlyCharge += bedToMidnight(bedTime);
+            nightlyCharge += midnightToEnd(endTime);
+
+        }
+
+        return nightlyCharge;
+    }
+
+    private int midnightToEnd(int endTime) {
+        return (endTime) * MIDNIGHT_TO_END;
+    }
+
+    private int bedToMidnight(int bedTime) {
+        return (MIDNIGHT - bedTime) * BED_TO_MIDNIGHT;
+    }
+
+    private int startToBedtime(int startTime, int bedTime) {
+        return (bedTime - startTime) * START_TO_BED;
+    }
+
+    private boolean timeIsValid(int startTime, int endTime) {
+        return isValidStartTime(startTime) && isValidateEndTime(endTime);
+    }
+
+    public Boolean isValidStartTime(int startTime) {
+
+        return startTime >= LOWER_START_TIME_THRESHOLD;
+    }
+
+    public Boolean isValidateEndTime(int endTime) {
+        return endTime <= UPPER_END_TIME_THRESHOLD;
+    }
 }
--- bsitterkata/src/test/java/TestBabySitter.java
+++ bsitterkata/src/test/java/TestBabySitter.java
@@ -1,13 +1,92 @@
-package test.java;
+import org.junit.Before;
+import org.junit.Test;
 
-/**
- * Created by matthewhouse on 4/14/16.
- */
+import static org.junit.Assert.*;
+
 public class TestBabySitter {
+
+    private BabySitter babySitter;
+
+    private int startTime;
+    private int bedTime;
+    private int endTime;
+    private int expectedCharge;
+    private int nightlyCharge;
+
+    @Before
+    public void setup() {
+        babySitter = new BabySitter();
+    }
 
     @Test
     public void babySitterExists() {
-        BabySitter babySitter = new BabySitter();
-        assertTrue(babySitter);
+        assertNotNull(babySitter);
     }
+
+    @Test
+    public void shiftMustStartAtFivePMOrLater() {
+        Boolean validTime = babySitter.isValidStartTime(17);
+
+        assertTrue(validTime);
+    }
+
+    @Test
+    public void babySitterCanNotStartBeforeFivePM() {
+        Boolean isValidTime = babySitter.isValidStartTime(16);
+
+        assertFalse(isValidTime);
+    }
+
+    @Test
+    public void babySitterCanNotStayPast4AM() {
+        Boolean isValidTime = babySitter.isValidateEndTime(5);
+
+        assertFalse(isValidTime);
+    }
+
+    @Test
+    public void babySitterStayUpUntil4AM() {
+        Boolean isValidTime = babySitter.isValidateEndTime(4);
+
+        assertTrue(isValidTime);
+    }
+
+    @Test
+    public void babySitterGetsPaid12DollarsAnHourFromStartToBedTime() {
+        startTime = 20;
+        bedTime = 22;
+        endTime = 2;
+        expectedCharge = 72;
+
+        nightlyCharge = babySitter.calculateNightlyCharge(startTime, bedTime, endTime);
+
+        assertEquals(expectedCharge, nightlyCharge);
+    }
+
+    @Test
+    public void babySitterGetsPaid8DollarsAnHourFromBedTimeToMidnight() {
+        startTime = 20;
+        bedTime = 22;
+        endTime = 2;
+        expectedCharge = 72;
+
+        nightlyCharge = babySitter.calculateNightlyCharge(startTime, bedTime, endTime);
+
+        assertEquals(expectedCharge, nightlyCharge);
+    }
+
+    @Test
+    public void babySitterGetsPaid16DollarsAnHourFromMidnightToEndOfShift() {
+        startTime = 20;
+        bedTime = 22;
+        endTime = 2;
+        expectedCharge = 72;
+
+        nightlyCharge = babySitter.calculateNightlyCharge(startTime, bedTime, endTime);
+
+        assertEquals(expectedCharge, nightlyCharge);
+    }
+
+
+
 }
